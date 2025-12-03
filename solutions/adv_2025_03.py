1accc7f89cad416833afcdffa40fa0727363efb4
vil02 <65706193+vil02@users.noreply.github.com>
2025-12-03 07:04:46 +0100

    feat: optimise `adv_2025_03`

--- solutions/adv_2025_03.py
+++ solutions/adv_2025_03.py
@@ -1,12 +1,11 @@
-import functools
 import typing
 
 
-def _parse_line(in_str: str) -> tuple[int, ...]:
-    return tuple(int(_) for _ in in_str)
+def _parse_line(in_str: str) -> list[int]:
+    return [int(_) for _ in in_str]
 
 
-def _parse_input(in_str: str) -> list[tuple[int, ...]]:
+def _parse_input(in_str: str) -> list[list[int]]:
     return [_parse_line(_) for _ in in_str.splitlines()]
 
 
@@ -16,22 +15,27 @@ def _join_joltages(first: int, rest: int, size: int) -> int:
     return res
 
 
-@functools.lru_cache(None)
-def max_joltage(bank: tuple[int, ...], size: int) -> int:
+def _pos_and_max(bank: list[int]) -> tuple[int, int]:
+    max_val = max(bank)
+    return bank.index(max_val), max_val
+
+
+def max_joltage(bank: list[int], size: int) -> int:
+    assert size > 0
     assert len(bank) >= size
-    if size == 0:
-        return 0
-    return max(
-        _join_joltages(
-            bank[first_pos], max_joltage(tuple(bank[first_pos + 1 :]), size - 1), size
-        )
-        for first_pos in range(len(bank) - size + 1)
-    )
+    if size == 1:
+        return max(bank)
+    cur_pos, cur_best = _pos_and_max(bank[: len(bank) - size + 1])
+    return _join_joltages(cur_best, max_joltage(bank[cur_pos + 1 :], size - 1), size)
+
+
+def _total_joltage(banks: list[list[int]], joltage_size: int) -> int:
+    return sum(max_joltage(_, joltage_size) for _ in banks)
 
 
 def _get_solve(joltage_size: int) -> typing.Callable[[str], int]:
     def _solve(in_str: str) -> int:
-        return sum(max_joltage(_, joltage_size) for _ in _parse_input(in_str))
+        return _total_joltage(_parse_input(in_str), joltage_size)
 
     return _solve
 

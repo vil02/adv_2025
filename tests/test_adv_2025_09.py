96cc930dc3372cee75519bc0685caa43dbcd5540
vil02 <65706193+vil02@users.noreply.github.com>
2025-12-09 16:54:33 +0100

    tests: add tests for `adv_2025_09`
    Source:
    r0: https://www.reddit.com/r/adventofcode/comments/1pi5rqn/2025_day_9_part_2_check_your_solution_with_this/
    r1-r3: https://www.reddit.com/r/adventofcode/comments/1pi36pq/2025_day_9_part_2_more_examples_to_soften_your/

--- tests/test_adv_2025_09.py
+++ tests/test_adv_2025_09.py
@@ -1,5 +1,13 @@
 from . import test_utils as tu
 
 test_solve_a, test_solve_b = tu.regular_tests(
-    __file__, {"small": (50, 24), "p": (4760959496, 1343576598)}
+    __file__,
+    {
+        "small": (50, 24),
+        "p": (4760959496, 1343576598),
+        "r_0": (180, 30),
+        "r_1": (10 * 9, 40),
+        "r_2": (10 * 9, 30),
+        "r_3": (16 * 16, 66),
+    },
 )

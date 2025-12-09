95044fad91030283284a0edfe972b79ba510366a
vil02 <65706193+vil02@users.noreply.github.com>
2025-12-09 07:39:19 +0100

    chore: simplify regular tests setup

--- tests/test_adv_2025_04.py
+++ tests/test_adv_2025_04.py
@@ -1,7 +1,5 @@
 from . import test_utils as tu
 
-sol = tu.import_solution(__file__)
-
-test_solve_a, test_solve_b = tu.get_inputs(__file__, {"small", "p"}).get_tests(
-    (sol.solve_a, sol.solve_b), {"small": (13, 43), "p": (1587, 8946)}
+test_solve_a, test_solve_b = tu.regular_tests(
+    __file__, {"small": (13, 43), "p": (1587, 8946)}
 )

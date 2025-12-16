6376e3a1ac34be93be582f2cf29542537afe6a6e
vil02 <65706193+vil02@users.noreply.github.com>
2025-12-16 19:45:04 +0100

    chore: add pyright

--- check_all_python_files.sh
+++ check_all_python_files.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 
 uv run ruff check solutions/ tests/
 uv run mypy --strict solutions/ tests/
+uv run pyright --warnings solutions/ tests/
 uv run isort --profile black --check solutions/ tests/
 find solutions/ tests/ -name "*.py" -exec ./check_python_file.sh {} +
